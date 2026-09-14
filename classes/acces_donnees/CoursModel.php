4fb0d024f7fc0a4f89e18e67fbedf842cb1085c6
Yehya LAABED <yehyaba51@gmail.com>
2026-09-14 15:53:45 +0200

    fix: add missing comma before exercices count in getCours query

--- classes/acces_donnees/CoursModel.php
+++ classes/acces_donnees/CoursModel.php
@@ -18,7 +18,7 @@ class CoursModel
                 c.cours_id,
                 c.cours_titre,
                 c.description,
-                CONCAT(u.prenom, ' ', u.nom) AS formateur
+                CONCAT(u.prenom, ' ', u.nom) AS formateur,
                 (SELECT COUNT(*) FROM exercice AS e WHERE e.cours_id = c.cours_id) AS exercices,
                 (SELECT COUNT(*) FROM lecon AS l WHERE l.cours_id = c.cours_id ) AS lecons
             FROM cours AS c
