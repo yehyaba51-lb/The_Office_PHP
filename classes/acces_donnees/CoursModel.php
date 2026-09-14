1a936f40418b8fedc929e5de8358c491138639c3
Yehya LAABED <yehyaba51@gmail.com>
2026-09-14 16:46:45 +0200

    Update CoursModel.php

--- classes/acces_donnees/CoursModel.php
+++ classes/acces_donnees/CoursModel.php
@@ -152,7 +152,7 @@ class CoursModel
 
     public function creerCours($data)
     {
-        if(empty($data['cours_titre']) || strlen(trim($data['cours_titre'])) < 2 || !preg_match('/^[A-ZÀ-ÿ][a-zA-ZÀ-ÿ0-9\' :\-]*$/u', $data['cours_titre'])){
+        if(empty($data['cours_titre']) || strlen(trim($data['cours_titre'])) < 2 || !preg_match('/^[A-ZÀ-ÿ][a-zA-ZÀ-ÿ0-9\' :\-]*$', $data['cours_titre'])){
             return false;
         }
         if(empty($data['formateur_id']) || $data['formateur_id'] === ""){
