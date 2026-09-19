9d1c419f776872eb7412816b896192971997cf98
yehyaba51-lb <yehyaba51@gmail.com>
2026-09-19 19:23:13 +0200

    feat: add choix endpoint returning choices by question id

---
+++ api/choix.php
@@ -0,0 +1,39 @@
+<?php
+    require_once(__DIR__ . '/../vendor/autoload.php');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+    $dotenv->load();
+
+    header('Access-Control-Allow-Origin: ' . $_ENV['FRONTEND_URL']);
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Allow-Headers: Content-Type');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+
+    require_once(__DIR__ . '/../classes/gestion_exercices/ExerciceManager.php');
+    
+    $manager = new ExerciceManager();
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+        exit;
+    } else if($_SERVER['REQUEST_METHOD'] === 'GET'){
+        if(!isset($_GET['id'])){
+            http_response_code(400);
+            echo json_encode(['error' => 'Id manquante']);
+            exit;
+        } else {
+            $allChoix = $manager->getChoixByQuestion($_GET['id']);
+
+            if($allChoix === false){
+                http_response_code(500);
+                echo json_encode(['error' => 'Impossible de récupérer les choix']);
+                exit;
+            }
+
+            http_response_code(200);
+            echo json_encode($allChoix);
+        }
+    } else {
+        http_response_code(405);
+        echo json_encode(['error' => 'Méthode non autorisée']);
+    }
