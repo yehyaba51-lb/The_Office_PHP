3a74ed486afa37811492f3ec714fc1a5633860c8
yehyaba51-lb <yehyaba51@gmail.com>
2026-09-24 17:40:15 +0200

    refactor: remove unused ordre property and accessors from ContenuLecon

--- classes/gestion_cours/ContenuLecon.php
+++ classes/gestion_cours/ContenuLecon.php
@@ -2,7 +2,6 @@
     abstract class ContenuLecon {
         protected $lecon_id;
         protected $cours_id;
-        protected $ordre;
 
         abstract public function afficherContenu();
 
@@ -22,11 +21,4 @@
             $this->cours_id = $cours_id;
         }
 
-        public function getOrdre(){
-            return $this->ordre;
-        }
-
-        public function setOrdre($ordre){
-            $this->ordre = $ordre;
-        }
     }
